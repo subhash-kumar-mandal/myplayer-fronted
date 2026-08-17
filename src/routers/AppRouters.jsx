6e9d0c33603261e864aeab2a688d84bbacff086d
subhash kumar <pm817405@gmail.com>
2026-08-17 14:52:55 +0530

    fix depoly

--- src/routers/AppRouters.jsx
+++ src/routers/AppRouters.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 
-import AdminLayout from '../layouts/Adminlayout';
+import AdminLayout from '@/layouts/AdminLayout';
 import PhoneSignUp from '../pages/PhoneSignup/PhoneSignUp'
 
 
